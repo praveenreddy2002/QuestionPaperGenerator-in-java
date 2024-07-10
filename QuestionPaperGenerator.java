cae5780b7033816dac3435dbd659596cb5b22e6a
Praveenreddy <126175030+praveenreddy2002@users.noreply.github.com>
2024-07-10 23:18:17 +0530

    Update and rename QuestionPaperGenerator4.java to QuestionPaperGenerator.java

--- QuestionPaperGenerator.java
+++ QuestionPaperGenerator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.io;
 
-public class QuestionPaperGenerator4 {
+public class QuestionPaperGenerator {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
